da0c2866f1f52d46b152c9477d37626065eae651
kns92 <kshampur@gmail.com>
2020-12-23 22:52:58 -0500

    modularization in progress - leaving notebooks

---
+++ degenerate_polygon.py
@@ -0,0 +1,121 @@
+from shapely.geometry.polygon import (Polygon, geos_geom_from_py)
+import numpy as np
+import math
+import random
+
+class DegeneratePolygon(Polygon):
+    
+    def __init__(self, shell=None, holes=None):
+        """
+        ::::THIS DOCSTRING COPIED FROM SHAPELY SRC CODE::::
+        
+        Parameters
+        ----------
+        shell : sequence
+            A sequence of (x, y [,z]) numeric coordinate pairs or triples.
+            Also can be a sequence of Point objects.
+        holes : sequence
+            A sequence of objects which satisfy the same requirements as the
+            shell parameters above
+        Example
+        -------
+        Create a square polygon with no holes
+          >>> coords = ((0., 0.), (0., 1.), (1., 1.), (1., 0.), (0., 0.))
+          >>> polygon = Polygon(coords)
+          >>> polygon.area
+          1.0
+        """
+        # this is all we need!
+        # just use Polygon classes init, which itself inherits
+        # from BaseGeometry
+
+        super().__init__(shell, holes)
+
+
+
+    def _update_shapely_polygon(self,shell=None,holes = None):
+        # copying factory of how it is done in shapely 1.7.1 src code, 
+        # soon deprecated btw 
+        ret = geos_polygon_from_py(shell = shell, holes = holes)
+        if ret is not None:
+            self._geom, self._ndim = ret
+        pass
+    
+    # general angle calculation has no allegiance to this class
+    @staticmethod
+    def get_angles(vec_1,vec_2):
+        dot = np.dot(vec_1, vec_2)
+        det = np.cross(vec_1,vec_2)
+        angle_in_rad = np.arctan2(det,dot)
+        return np.degrees(angle_in_rad)
+    
+    def simplify_by_interior_angle(self, deg_tol = 1):
+        """
+        try to remove persistent coordinate points that remain after
+        simplify, convex hul, etc with some trig
+        params:
+        
+        deg_tol = the degree tolerance, in Angles, for which to elimnate points
+                i.e. if deg_tol = 1, remove any point (x_n,y_n), whose angle 
+                between vector <x_n+1-x_n,y_n+1-y_n> and <x_n-x_n-1,y_n-y_n-1> 
+                is less than deg_tol. Default set to 1 for use case of eliminating
+                degenerate points
+                
+
+        """
+
+        ext_poly_coords = self.exterior.coords[:]
+        vector_rep = np.diff(ext_poly_coords,axis = 0)
+        angles_list = []
+        for i in range(0,len(vector_rep) -1 ):
+            angles_list.append(np.abs(get_angles(vector_rep[i],vector_rep[i+1])))
+
+
+        thresh_vals_by_deg = np.where(np.array(angles_list) > deg_tol)
+    #   gotta be a better way to do this. sandwich betweens first and last pts
+        new_idx = [0] + (thresh_vals_by_deg[0] + 1).tolist() + [0]
+        new_vertices = [ext_poly_coords[idx] for idx in new_idx]
+        
+        self._update_shapely_polygon(shell=new_vertices)
+
+
+
+
+    
+    def make_degenerate(self, pps = 5):
+        """
+        Params
+        pps : integer of amount of random points to make per side i.e. edge between
+            vertices
+        return: modified polygon with new rand points
+        """
+        coords = self.exterior.coords[:]
+        coords_arr = np.array(coords)
+        vector_rep = np.diff(coords_arr,axis = 0)
+        distances = np.linalg.norm(vector_rep,axis = 1)
+
+        idx_dict = {"distance":0, 
+                   "vector":1,
+                   "coords":2}
+
+        final_coords=[]
+        dist_scaler = 10
+        for e,d_v_c in enumerate(zip(distances,vector_rep,coords_arr)):
+            d= d_v_c[idx_dict['distance']]
+            v = d_v_c[idx_dict['vector']]
+            c = d_v_c[idx_dict['coords']]
+
+            u = v/d
+            # side_pts = np.random.randint(1,d*10,(pps))/11 # dont want a chance of zero 
+            side_pts = random.sample(range(1, math.floor(d*dist_scaler)), pps)
+
+            # maintain proper sequence by sorting distance scalars and prepending starting pt of current vector
+            side_pts.sort() 
+            new_offset = [c + u*du/dist_scaler for du in side_pts]
+            final_coords+= [c] + new_offset #+ list(coords[e+1])
+            
+
+
+        self._update_shapely_polygon(shell = final_coords)
+            
+        pass
---
+++ geom_utils.py
@@ -0,0 +1,84 @@
+import shapely.geometry as sg
+import shapely
+import numpy as np
+import random
+import math
+import pandas as pd
+
+# collection of random funcs.
+# makes pretty heavy use of shapely geometry methods
+
+# pickling---------------------------------------------------
+def update_poly_pickle(poly, return_df=False):
+    try:
+        df_poly = pd.read_pickle('./randpolys.pkl')
+    except:
+#         just make a new df
+        print('making new poly df...')
+        df_poly = make_poly_df()
+
+#     get next loc
+    df_idx_next = len(df_poly)
+    df_poly.loc[df_idx_next] = poly.wkt
+    df_poly = df_poly.drop_duplicates(ignore_index=True)
+
+    df_poly.to_pickle('./randpolys.pkl')
+    
+    print('done')
+    if return_df:
+        return df_poly
+    
+def make_poly_df():
+    poly_df = pd.DataFrame(columns=['poly_wkt'])
+    poly_df.to_pickle('./randpolys.pkl')
+    return poly_df
+
+# visualization & polygon related---------------------------
+
+def poly_as_mpt(polygon):
+    coords = polygon.exterior.coords[:]
+    as_pts = [sg.Point(x) for x in coords]
+    as_mpt = sg.MultiPoint(as_pts)
+    return as_mpt
+
+def make_hex_poly(a, x, y):
+    """
+    hexagon centered on (x, y)
+    :param a: length of the hexagon's edge
+    :param x,y: desired centroid of hex
+    :return: The shapely polygon representation of the hexagon
+    """
+    hex_coords = [(x + math.cos(math.radians(angle)) * a, y + math.sin(math.radians(angle)) * a) for angle in range(0, 360, 60)]
+    return sg.Polygon(hex_coords)
+
+def generate_random_polygon( n):
+
+    # given n verticies, generate a random non-zero area, non-self intersecting poly
+
+    x = np.random.randint(0,50,n)
+    y = np.random.randint(0,50,n)
+    centroid = [np.sum(x)/n, np.sum(y)/n]
+
+    angles = np.arctan2(x-centroid[0], y-centroid[1])
+
+    ##sorting the points by angle
+    coords_by_theta = sorted([((xx,yy),theta) for xx,yy,theta in zip(x,y,angles)], key = lambda t: t[1])
+
+    xy,angles = zip(*coords_by_theta)
+#     append first point to end for complete linear ring sequence
+#     xy.append(xy[0])
+    xy = list(xy)
+    xy.append(xy[0])
+    return xy
+
+
+def buffer_poly_vtx(poly,buffer=1):
+    pts = poly.exterior.coords[:]
+    buffpts = [sg.Point(x).buffer(1) for x in pts]
+    buffpts = sg.MultiPolygon(buffpts)
+    poly_ext = poly.exterior
+#     return geom collection
+    try:
+        return poly_ext.union(buffpts)    
+    except Exception as e:
+        print(e)
